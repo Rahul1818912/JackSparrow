70a828a0bcfcb695f3b229a274d8248a973e06d3
Jack Sparrow <55913263+userbotpapa9838@users.noreply.github.com>
2019-10-14 15:37:25 +0530

    Update deploy.py

--- stdplugins/deploy.py
+++ stdplugins/deploy.py
@@ -32,9 +32,9 @@ async def _(event):
 
         animation_chars = [
         
-            "**Heroku Connecting To Latest Github Build (Pornhub)**",
-            "**Build started by user** @r4v4n4",
-            "**Deploy** `535a74f0` **by user** @r4v4n4",
+            "**Heroku Connecting To Latest Github Build (JackSparrow)**",
+            "**Build started by user** @iam_captain_jack",
+            "**Deploy** `535a74f0` **by user** @iam_captain_jack",
             "**Restarting Heroku Server...**",
             "**State changed from up to starting**",    
             "**Stopping all processes with SIGTERM**",
