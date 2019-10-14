a77696d9f012973ac68f95e82d4840de34df37b7
Jack Sparrow <55913263+userbotpapa9838@users.noreply.github.com>
2019-10-14 16:12:45 +0530

    Update profilepicgrab.py

--- stdplugins/profilepicgrab.py
+++ stdplugins/profilepicgrab.py
@@ -46,9 +46,9 @@ async def _(event):
     except Exception as e:
         dc_id = "Need a Profile Picture to check **this**"
         location = str(e)
-    caption = """Profile Pic Grabber By @r4v4n4
+    caption = """Profile Pic Robbed By @iam_captain_jack
 
-Rendi: <a href='tg://user?id={}'>{}</a>
+User_id: <a href='tg://user?id={}'>{}</a>
 """.format(
         user_id,
         user_id,
